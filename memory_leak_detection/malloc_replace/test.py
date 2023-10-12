18c89fbc8c7670d9113c0aa3132476933429df85
Tomonori Shimomura <shimomut@amazon.com>
2023-10-12 16:32:16 +0000

    Python based performance testing

--- memory_leak_detection/malloc_replace/test.py
+++ memory_leak_detection/malloc_replace/test.py
@@ -1,3 +1,15 @@
-import socket
+import random
 
-print("Hello")
+d = []
+
+for i in range(100000):
+    size = random.randint( 1, 1024 )
+    item = ["a"] * size
+    d.append(item)
+
+while d:
+    i = random.randint( 0, len(d)-1 )
+    del d[i]
+
+print("Done")
+
